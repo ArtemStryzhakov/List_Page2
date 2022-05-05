416379fd251d603a03d9ee534061d5588fdb29e8
ArtemStryzhakov <77277962+ArtemStryzhakov@users.noreply.github.com>
2022-05-05 21:53:31 +0300

    Add files via upload

--- Elemendide_App/List_Page.xaml.cs
+++ Elemendide_App/List_Page.xaml.cs
@@ -16,10 +16,7 @@ namespace Elemendide_App
         Label lbl_list;
         ListView List;
         Button lisa, kustuta;
-        Entry en;
-        MediaFile file;
-        string imageName;
-        string filePath;
+
         public List_Page()
         {
             telefons = new ObservableCollection<Telefon>
@@ -39,7 +36,6 @@ namespace Elemendide_App
             {
                 SeparatorColor = Color.Orange,
                 Header = "Minu oma kolektion",
-                Footer = DateTime.Now.ToString("T"),
 
                 HasUnevenRows = true,
                 ItemsSource = telefons,
@@ -64,67 +60,15 @@ namespace Elemendide_App
             this.Content = new StackLayout { Children = { lbl_list, List, lisa, kustuta } };
         }
 
-        private ObservableCollection<MediaFile> _images;
-        public ObservableCollection<MediaFile> Images
-        {
-            get { return _images ?? (_images = new ObservableCollection<MediaFile>()); }
-            set
-            {
-                if (_images != value)
-                {
-                    _images = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Allows the user to pick a photo on their device using the native photo handlers and returns a stream, which we save to disk.
-        /// </summary>
-        /// <returns>string, the name of the image if everything went ok, 'false' if an exception was generated, and 'notfalse' if they simply canceled.</returns>
-        public async Task<string> PickPictureAsync()
-        {
-
-            file = null;
-            filePath = string.Empty;
-            imageName = string.Empty;
-
-            try
-            {
-                file = await CrossMedia.Current.PickPhotoAsync();
-
-                #region Null Check
-
-                if (file == null) { return null; }                                                                                 //Not returning false here so we do not show an error if they simply hit cancel from the device image picker
-
-                #endregion
-
-                imageName = "SomeImageName.jpg";
-
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine("\nIn PictureViewModel.PickPictureAsync() - Exception:\n{0}\n", ex);                           //TODO: Do something more useful
-                return null;
-            }
-            finally { if (file != null) { file.Dispose(); } }
-
-
-
-            return imageName;
-        }
         private async void Lisa_Clicked(object sender, EventArgs e)
         {
 
-            //telefons.Add(new Telefon { Nimetus = "Telefon", Tootja = "Tootja", Hind = 1 });
-            string site = await DisplayPromptAsync("Kakoi telefon ti hotes dobavit?", "Napishi ego", keyboard: Keyboard.Text);
+            string nameOfPhone = await DisplayPromptAsync("What name of this phone?", "Write it down: ", keyboard: Keyboard.Text);
+            string nameOfCompany = await DisplayPromptAsync("What name company of this phone?", "Write it down:", keyboard: Keyboard.Text);
+            string cost = await DisplayPromptAsync("What cost of the phone?", "Write it down:", keyboard: Keyboard.Numeric);
+            string phoneLink = await DisplayPromptAsync("Write a link of the image?", "Write it down:", keyboard: Keyboard.Text);
 
-            string site2 = await DisplayPromptAsync("Kakoi telefon ti hotes dobavit?", "Napishi ego", keyboard: Keyboard.Text);
-
-            string site3 = await DisplayPromptAsync("Kakoi tsena?", "Napishi ego", keyboard: Keyboard.Numeric);
-            PickPictureAsync();
-            string site4 = imageName;
-            telefons.Add(item: new Telefon { Nimetus = site, Tootaja = site2, Hind = site3, Pilt = site4 });
+            telefons.Add(item: new Telefon { Nimetus = nameOfPhone, Tootaja = nameOfCompany, Hind = cost, Pilt = phoneLink });
 
         }
 
@@ -144,7 +88,5 @@ namespace Elemendide_App
             if (selectedPhone != null)
                 await DisplayAlert("Выбранная модель", $"{selectedPhone.Tootaja}-{selectedPhone.Nimetus}", "OK");
         }
-
-
     }
 }
